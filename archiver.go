f1f1b3a685274d5abd1abc4db366c44f4e7b18c0
tomocy <tomocy.dev@gmail.com>
2018-08-22 00:21:02 +0900

    add type for zip and make it implement Archiver

--- archiver.go
+++ archiver.go
@@ -1,5 +1,55 @@
 package backup
 
+import (
+	"archive/zip"
+	"io"
+	"os"
+	"path/filepath"
+)
+
 type Archiver interface {
 	Archive(src, dest string) error
+}
+
+var ZIP Archiver = new(zipper)
+
+type zipper struct {
+}
+
+func (z zipper) Archive(src, dest string) error {
+	if err := os.MkdirAll(filepath.Dir(dest), 0777); err != nil {
+		return err
+	}
+	out, err := os.Create(dest)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	w := zip.NewWriter(out)
+	defer w.Close()
+
+	return filepath.Walk(src, func(path string, info os.FileInfo, err error) error {
+		if info.IsDir() {
+			return nil
+		}
+
+		if err != nil {
+			return err
+		}
+
+		in, err := os.Open(path)
+		if err != nil {
+			return err
+		}
+		defer in.Close()
+
+		f, err := w.Create(path)
+		if err != nil {
+			return err
+		}
+		io.Copy(f, in)
+
+		return nil
+	})
 }
