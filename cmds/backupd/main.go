df0c2084fed768a88e4639f198e0d6d4ceada0ae
tomocy <tomocy.dev@gmail.com>
2018-08-23 17:38:53 +0900

    add type for path in db

---
+++ cmds/backupd/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"log"
+)
+
+type path struct {
+	Path string
+	Hash string
+}
+
+func main() {
+	var fatalErr error
+	defer func() {
+		if fatalErr != nil {
+			log.Fatalln(fatalErr)
+		}
+	}()
+}
