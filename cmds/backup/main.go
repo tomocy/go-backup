377cd8b739f1f3c74d5846113a705d567e5abac7
tomocy <tomocy.dev@gmail.com>
2018-08-23 22:53:50 +0900

    make it use db instead

--- cmds/backup/main.go
+++ cmds/backup/main.go
@@ -1,14 +1,10 @@
 package main
 
 import (
-	"encoding/json"
-	"errors"
 	"flag"
 	"fmt"
 	"log"
 	"strings"
-
-	"github.com/matryer/filedb"
 )
 
 func main() {
@@ -22,67 +18,53 @@ func main() {
 	dbPath := flag.String("db", "./backup", "the path to db folder")
 	flag.Parse()
 
-	dbSession, err := filedb.Dial(*dbPath)
-	if err != nil {
-		fatalErr = err
-		return
-	}
-	defer dbSession.Close()
-
-	paths, err := dbSession.C("paths")
-	if err != nil {
-		fatalErr = err
-		return
-	}
+	db := newDB()
+	db.open(*dbPath)
+	defer db.close()
 
 	args := flag.Args()
 	subCmd := strings.ToLower(args[0])
 	switch subCmd {
 	case "list":
-		paths.ForEach(func(i int, data []byte) bool {
-			var file monitoredFile
-			if err := json.Unmarshal(data, &file); err != nil {
-				fatalErr = err
-				return true
-			}
-
-			fmt.Printf("= %s\n", file)
-			return false
-		})
+		listMonitoredFiles(db)
 	case "add":
-		newPaths := flag.Args()[1:]
-		if len(newPaths) < 1 {
-			fatalErr = errors.New("nothing specified, nothing added")
-			return
-		}
-		for _, newPath := range newPaths {
-			pathJSON := &monitoredFile{
-				path: newPath,
-				hash: "not hashed yet",
-			}
-			if err := paths.InsertJSON(pathJSON); err != nil {
-				fatalErr = err
-				return
-			}
-
-			fmt.Printf("+ %s\n", pathJSON)
-		}
+		addFileToMonitoredFileList(db)
 	case "remove":
-		paths.RemoveEach(func(i int, data []byte) (bool, bool) {
-			var file monitoredFile
-			if err := json.Unmarshal(data, &file); err != nil {
-				fatalErr = err
-				return false, true
-			}
-
-			pathsToBeRemoved := flag.Args()[1:]
-			for _, pathToBeRemoved := range pathsToBeRemoved {
-				if pathToBeRemoved == file.path {
-					return true, false
-				}
-			}
-
-			return false, false
-		})
+		removeMonitoredFiles(db)
 	}
+}
+
+func listMonitoredFiles(db db) {
+	files, err := db.fileList()
+	if err != nil {
+		log.Printf("could not get file list: %s\n", err)
+		return
+	}
+
+	for _, file := range files {
+		fmt.Printf("- %s", file)
+	}
+}
+
+func addFileToMonitoredFileList(db db) {
+	fileNames := flag.Args()[1:]
+	if len(fileNames) < 1 {
+		log.Println("nothing specified, nothing added")
+		return
+	}
+	db.addFiles(fileNames...)
+
+	for _, fileName := range fileNames {
+		fmt.Printf("+ %s", fileName)
+	}
+}
+
+func removeMonitoredFiles(db db) {
+	fileNames := flag.Args()[1:]
+	if len(fileNames) < 1 {
+		log.Println("nothing specified, nothing removed")
+		return
+	}
+
+	db.removeFiles(fileNames...)
 }
