d39a6f315dd0a14286273537f880f62b3cb7a24b
tomocy <tomocy.dev@gmail.com>
2018-08-23 18:06:09 +0900

    default backupd

--- cmds/backupd/main.go
+++ cmds/backupd/main.go
@@ -4,7 +4,12 @@ import (
 	"encoding/json"
 	"errors"
 	"flag"
+	"fmt"
 	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/matryer/filedb"
 	"github.com/tomocy/backup"
@@ -25,6 +30,7 @@ func main() {
 
 	dest := flag.String("dest", "./archive", "the path to folder backup file is archived")
 	dbPath := flag.String("db", "./db", "the path to db")
+	interval := flag.Duration("interval", 10, "the interval of backup (unit: second)")
 	flag.Parse()
 
 	monitor := &backup.Monitor{
@@ -62,4 +68,48 @@ func main() {
 		fatalErr = errors.New("no paths specified. add paths with backup cmd")
 		return
 	}
+
+	check(monitor, paths)
+	signalCh := make(chan os.Signal)
+	signal.Notify(signalCh, syscall.SIGINT)
+	ticker := time.NewTicker(*interval * time.Second)
+	defer ticker.Stop()
+	for {
+		select {
+		case <-ticker.C:
+			check(monitor, paths)
+		case <-signalCh:
+			fmt.Println()
+			fmt.Println("stopped backupping")
+			return
+		}
+	}
+}
+
+func check(m *backup.Monitor, col *filedb.C) {
+	backupCnt, err := m.Now()
+	if err != nil {
+		log.Panicf("faild to backup: %s\n", err)
+	}
+	if backupCnt < 1 {
+		log.Println("nothing to be backupped")
+		return
+	}
+
+	col.SelectEach(func(_ int, data []byte) (bool, []byte, bool) {
+		var path path
+		if err := json.Unmarshal(data, &path); err != nil {
+			log.Printf("faild to unmarshal json: %s\n", err)
+			return true, data, false
+		}
+
+		path.Hash = m.Hashs[path.Path]
+		newData, err := json.Marshal(path)
+		if err != nil {
+			log.Printf("faild to marshal json: %s\n", err)
+			return true, data, false
+		}
+
+		return true, newData, false
+	})
 }
